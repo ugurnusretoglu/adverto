e58e1d0a796c6c6381338621850424a55b4f72f2
ugurnusretoglu <ugurnusretoglu2004@gmail.com>
2026-04-01 17:11:31 +0300

    Backend project initialized

---
+++ adverto/src/main/java/com/ugur/adverto/AdvertoApplication.java
@@ -0,0 +1,13 @@
+package com.ugur.adverto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertoApplication.class, args);
+	}
+
+}
---
+++ adverto/src/test/java/com/ugur/adverto/AdvertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugur.adverto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
